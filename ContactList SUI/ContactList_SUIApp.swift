f4e8a72f0e36980d09a05d7121257a67ed601a41
Sav1taRG <52829649+Sav1taRG@users.noreply.github.com>
2022-12-24 19:19:16 +0200

    Initial Commit

---
+++ ContactList SUI/ContactList_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SUIApp.swift
+//  ContactList SUI
+//
+//  Created by Roman Golubinko on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
